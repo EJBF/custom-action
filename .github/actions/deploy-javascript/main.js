f4de9945fa4bbd33fbae282eae0a4c99e200713b
ejbf <eduardofaria.mail@sapo.pt>
2025-03-12 16:32:15 +0000

    JavaScript Action

--- .github/actions/deploy-javascript/main.js
+++ .github/actions/deploy-javascript/main.js
@@ -1,7 +1,7 @@
 // npm install @actions/core @actions/github @actions/exec
 
 const core = require('./node_modules/@actions/core');
-const github = require('./node_modules/github');
+const github = require('./node_modules/@actions/github');
 const exec = require('./node_modules/@actions/exec');
 
 function run() {
