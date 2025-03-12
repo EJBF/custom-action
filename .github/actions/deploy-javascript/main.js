012890489b0b23472b5200b8cdbe7240384791fd
ejbf <eduardofaria.mail@sapo.pt>
2025-03-12 16:29:43 +0000

    JavaScript Action

--- .github/actions/deploy-javascript/main.js
+++ .github/actions/deploy-javascript/main.js
@@ -1,8 +1,8 @@
 // npm install @actions/core @actions/github @actions/exec
 
-const core = require('@actions/core');
-const github = require('github');
-const exec = require('@actions/exec');
+const core = require('./node_modules/@actions/core');
+const github = require('./node_modules/github');
+const exec = require('./node_modules/@actions/exec');
 
 function run() {
   core.notice('Hello from the custom JavaScript Action!');
